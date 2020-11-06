b6e8090e58b1acd192f4079431231eddeb325a2b
robot5000-design <ducativtwin@gmail.com>
2020-11-06 00:11:39 +0000

    Added partial next question functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 //document.addEventListener("DOMContentLoaded", function () {
 $(document).ready(function() {
-   
-  
-    let amount, category, difficulty, questionSet;
+    // declare variables  
+    let amount,
+        category, 
+        difficulty, 
+        questionSet;
 
     let categoryButtons = $(".categories").children("button");
     let difficultyButtons = $(".difficulty-level").children("button");
     let quantityButtons = $(".question-quantity").children("button");
     
-    
-        //console.log(buttons);
     for (let singleButton of categoryButtons) {        
         if ($(singleButton).hasClass("active")) {
             category = singleButton.getAttribute("data-value");                        
@@ -29,8 +29,8 @@ $(document).ready(function() {
         console.log(amount);
     }
 
-    function makeUrl() {
-        
+    // sets the url for downloading the chosen question options
+    function makeUrl() {        
     /* let quantityButtons = document.querySelectorAll(".question-quantity-button");
         quantityButtons.forEach(function(quantityButton) {
             quantityButton.addEventListener("click", function() {
@@ -55,30 +55,52 @@ $(document).ready(function() {
         return `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}`;
     }
 
+    // switch off quiz options and switch on questions
     function toggleOptions() {
         if ($(".question-options").css("display") != "none") {
-            $(".question-options").css("display", "none");
-            $(".question-container").css("display", "block");
+            $(".question-options").removeClass("reinstate-element").addClass("delete-element");
+            $(".question-container").removeClass("delete-element").addClass("reinstate-element");
         } else {
-            $(".question-options").css("display", "block");
-            $(".question-container").css("display", "none");
+            $(".question-options").removeClass("delete-element").addClass("reinstate-element");
+            $(".question-container").removeClass("reinstate-element").addClass("delete-element");
         }
     }
 
-    function populateQuestions(questionSet) {
+    function askQuestions(setOfQuestions, i) {
         let currentQuestion,
             correctAnswer,
             wrongAnswers,
             currentType;
-        toggleOptions();
-        
-        for (let i = 0; i < questionSet.length; i++) {
-            currentQuestion = questionSet[i].question
 
-            document.getElementsByClass("questions").innerText = (currentQuestion);
-        }
+        // check if question is boolean and if yes, hide redundant answer buttons
+        if (setOfQuestions[i].type == "boolean") {
+            $("[data-number='3']").addClass("hide-element");
+            $("[data-number='4']").addClass("hide-element");
+        } else {
+            $("[data-number='3']").removeClass("hide-element");
+            $("[data-number='4']").removeClass("hide-element");
+        }            
+        currentQuestion = setOfQuestions[i].question;
+        //console.log(currentQuestion, i);
+        document.getElementsByClassName("questions")[0].innerHTML = `${i+1}. ${currentQuestion}`;        
+        i++;
+        console.log(i);   
+        $(".next-question").click(function() {
+            if (i < setOfQuestions.length) {
+                askQuestions(setOfQuestions, i);
+            } else {
+                toggleOptions();
+            }
+        })
+    } 
+
+    // populate the questions and answers in html elements and run quiz
+    function populateQuestions(setOfQuestions) {
+        toggleOptions();
+        askQuestions(setOfQuestions, 0);
     }
 
+    // get the quiz dataset from opentdb api
     function getData(apiUrl) {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", apiUrl);
@@ -97,20 +119,22 @@ $(document).ready(function() {
         };
     }
 
-  function printDataToConsole(data) {
-    console.log(data);
-    console.log(data.response_code);
-    console.log(typeof(data));
-  }
+    function printDataToConsole(data) {
+        console.log(data);
+        console.log(data.response_code);
+        console.log(typeof(data));
+    }
 
-  $(".load-questions").click(function() {
-      getData(makeUrl());
-      console.log(makeUrl());
-  });
- // getData(printDataToConsole);
+    $(".load-questions").click(function() {
+        getData(makeUrl());
+        console.log(makeUrl());
+    });
+    // getData(printDataToConsole);
 
-  $("body").on("click", ".list-group .btn", function () {
-    $(this).addClass("active");
-    $(this).siblings().removeClass("active");
-  });
+    // with help from https://stackoverflow.com/questions/29128228/multiple-list-groups-on-a-single-page-but-each-list-group-allows-an-unique-sele
+    // separates the multiple bootstrap list groups on the same page
+    $("body").on("click", ".list-group .btn", function () {
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
+    });
 });
